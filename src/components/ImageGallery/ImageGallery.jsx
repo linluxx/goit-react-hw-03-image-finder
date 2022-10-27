5b1e1d30d27a9ac7d49d5fa14c6ea73a30e45641
linluxx <karolina.kkv@gmail.com>
2022-10-27 22:10:34 +0300

    add axios config

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,13 +3,14 @@ import { Button } from 'components/Button/Button';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Gallery } from './ImageGallery.styled';
 import { Loader } from 'components/Loader/Loader';
-import axios from 'axios';
+import { getData } from 'components/Api/AxiosConfig';
+// import axios from 'axios';
 import PropTypes from 'prop-types';
 
-const KEY = '29800147-042a8c86586ab835e1f8a2965';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const SEARCH_PARAMS =
-  'image_type=photo&orientation=horizontal&safesearch=true&per_page=12';
+// const KEY = '29800147-042a8c86586ab835e1f8a2965';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const SEARCH_PARAMS =
+//   'image_type=photo&orientation=horizontal&safesearch=true&per_page=12';
 
 export class ImageGallery extends Component {
   state = {
@@ -18,15 +19,21 @@ export class ImageGallery extends Component {
     isLoading: false,
   };
 
-  async componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
+    const {
+      props: { searchQuery },
+      state: { page },
+      showLoader,
+      hideLoader,
+    } = this;
     if (prevProps.searchQuery !== this.props.searchQuery) {
       this.setState({ page: 1, data: [] });
-      this.getData().then(res => {
+      getData(searchQuery, page, showLoader, hideLoader).then(res => {
         this.setState({ data: res });
       });
     }
-    if (prevState.page !== this.state.page) {
-      this.getData().then(res => {
+    if (prevState.page !== page) {
+      getData(searchQuery, page, showLoader, hideLoader).then(res => {
         this.setState(({ data }) => ({
           data: [...data, ...res],
         }));
@@ -34,43 +41,35 @@ export class ImageGallery extends Component {
     }
   }
 
-  getData = async () => {
-    try {
-      this.toggleLoader();
-      const response = await axios.get(
-        `?key=${KEY}&q=${this.props.searchQuery}&${SEARCH_PARAMS}&page=${this.state.page}`
-      );
-      this.toggleLoader();
-      return response.data.hits;
-    } catch (error) {
-      console.error(error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
   onLoadMoreClick = () => {
     this.setState(prevState => ({
       page: prevState.page + 1,
     }));
   };
 
-  toggleLoader = () => {
-    this.setState(state => ({ isLoading: !state.isLoading }));
+  showLoader = () => {
+    this.setState({ isLoading: true });
+  };
+
+  hideLoader = () => {
+    this.setState({ isLoading: false });
   };
 
   render() {
+    const {
+      onGalleryListClick,
+      onLoadMoreClick,
+      state: { data, isLoading },
+    } = this;
     return (
       <>
-        {this.state.isLoading && <Loader />}
-        <Gallery onClick={this.onGalleryListClick}>
-          {this.state.data.map(img => {
+        {isLoading && <Loader />}
+        <Gallery onClick={onGalleryListClick}>
+          {data.map(img => {
             return <ImageGalleryItem data={img} key={img.id} />;
           })}
         </Gallery>
-        {this.state.data.length > 11 && (
-          <Button onLoadMoreClick={this.onLoadMoreClick} />
-        )}
+        {data.length > 11 && <Button onLoadMoreClick={onLoadMoreClick} />}
       </>
     );
   }
